9c20853cc1734f0db6f55d2e870019871980115f
Pei <ucabpc5@ucl.ac.uk>
2025-06-24 22:44:48 +0100

    feat(time-simulation): add backend folder and implement frontend energy forecast API call

--- app/src/main/java/com/ucl/energygrid/MainActivity.kt
+++ app/src/main/java/com/ucl/energygrid/MainActivity.kt
@@ -4,21 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.ucl.energygrid.ui.screen.MainScreen
-import com.ucl.energygrid.data.fetchFloodPolygons
-import com.ucl.energygrid.data.loadPolygonPoints
-import com.ucl.energygrid.data.loadCenterPoint
-import androidx.compose.material3.Text
-import androidx.lifecycle.lifecycleScope
-import android.util.Log
-import kotlinx.coroutines.launch
-import com.ucl.energygrid.data.readAndExtractSitesByType
-import com.ucl.energygrid.data.GeoJsonLoader
-import com.ucl.energygrid.data.loadEnergyConsumption
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import android.content.Context
-import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVParser
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
