6d3c2af654195812b65317471d5dd7e32ba12de4
Pei <ucabpc5@ucl.ac.uk>
2025-07-06 14:05:56 +0100

    feat(auth): add AuthViewModel to track login state

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
     // Google Maps Compose SDK
     implementation("com.google.maps.android:maps-compose:$mapsComposeVersion")
     implementation("com.google.maps.android:maps-compose-utils:$mapsComposeVersion")
