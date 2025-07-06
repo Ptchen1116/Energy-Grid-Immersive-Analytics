58988188e4ccce13fced0b5b01c94ac23fb86437
Pei <ucabpc5@ucl.ac.uk>
2025-07-06 19:51:49 +0100

    feat(auth): add login state-aware dropdown with 'My Pins' menu

--- app/src/main/java/com/ucl/energygrid/data/API/AuthViewModel.kt
+++ app/src/main/java/com/ucl/energygrid/data/API/AuthViewModel.kt
@@ -1,32 +1,31 @@
 package com.ucl.energygrid.data.API
 
-// ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 
-// Compose State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-
 class AuthViewModel : ViewModel() {
-    var isLoggedIn by mutableStateOf(false)
-        private set
 
-    var userToken by mutableStateOf<String?>(null)
-        private set
+    private var _isLoggedIn = mutableStateOf(false)
+    val isLoggedIn: State<Boolean> get() = _isLoggedIn
 
-    var userId by mutableStateOf<String?>(null)
-        private set
+    private var _userToken = mutableStateOf<String?>(null)
+    val userToken: State<String?> get() = _userToken
+
+    private var _userId = mutableStateOf<String?>(null)
+    val userId: State<String?> get() = _userId
 
     fun loginSuccess(token: String, id: String) {
-        isLoggedIn = true
-        userToken = token
-        userId = id
+        _isLoggedIn.value = true
+        _userToken.value = token
+        _userId.value = id
     }
 
     fun logout() {
-        isLoggedIn = false
-        userToken = null
-        userId = null
+        _isLoggedIn.value = false
+        _userToken.value = null
+        _userId.value = null
     }
 }
