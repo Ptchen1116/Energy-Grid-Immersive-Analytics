89081c4fe57a096435c7a7a71aae6c2e1627ef20
Pei <ucabpc5@ucl.ac.uk>
2025-07-25 18:47:48 +0100

    chore: remove unused API/AuthViewModel.kt

--- app/src/main/java/com/ucl/energygrid/ui/component/Pin.kt
+++ app/src/main/java/com/ucl/energygrid/ui/component/Pin.kt
@@ -10,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.graphics.toColorInt
 import com.ucl.energygrid.R
 import com.ucl.energygrid.data.model.PinType
 import com.ucl.energygrid.data.model.Trend
