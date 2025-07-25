1a4cd4d59348696b5aed6b7f742471b3b07ce9e4
Pei <ucabpc5@ucl.ac.uk>
2025-07-25 17:37:16 +0100

    refactor(MainScreen): restructure into MVVM architecture

---
+++ app/src/main/java/com/ucl/energygrid/ui/screen/AuthViewModel.kt
@@ -0,0 +1,29 @@
+package com.ucl.energygrid.ui.screen
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    private var _isLoggedIn = mutableStateOf(false)
+    val isLoggedIn: State<Boolean> get() = _isLoggedIn
+
+    private var _userToken = mutableStateOf<String?>(null)
+    val userToken: State<String?> get() = _userToken
+
+    private var _userId = mutableStateOf<String?>(null)
+    val userId: State<String?> get() = _userId
+
+    fun loginSuccess(token: String, id: String) {
+        _isLoggedIn.value = true
+        _userToken.value = token
+        _userId.value = id
+    }
+
+    fun logout() {
+        _isLoggedIn.value = false
+        _userToken.value = null
+        _userId.value = null
+    }
+}
